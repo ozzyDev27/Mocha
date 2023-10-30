73230feee16dff7231ddde420540c91f182d8478
ozzy <businessozzy27@gmail.com>
2023-10-30 10:45:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,49 +2,63 @@ import time
 import math
 import os
 
-
 # ----------------------------------- Setup ---------------------------------- #
-files={} #nodeID:(filetype,otherpropertieshere,data)
 location=0 #would be a node ID - 0 is the root node's ID
 IDs=[] #list of all node IDs
+
 def error(msg): #? def a useful function trust me
     print(msg) #will probably eventually become useful in some way
 
 class Branch:
     def __init__(self, nodeID, parentID):
         global IDs
-        self.nodeID = nodeID #pointer to file object
+        self.nodeID = nodeID #how u can identify the class
         IDs.append(nodeID)
+        self.properties = None #something here eventually
+        self.data = None #would have the file data
         self.children = []
         self.parent = parentID
 
     def addChild(self, childNode):
-        self.children.append(childNode)
+        global IDs
+        if childNode in IDs:
+            self.children.append(childNode)
+        else: error(f"ID Error: {childNode} is not a Valid ID!")
+            
     
     def removeChild(self,childNode):
         self.children = [child for child in self.children if child is not childNode]
-
+    
+    def changeParent(self,parentNode):
+        global IDs
+        if parentNode in IDs:
+            self.parent=parentNode
+        else: error(f"ID Error: {parentNode} is not a Valid ID!")
+            
     def traverseUp(self, nodeID):
         global location
         if type(self.parent)==int:
             location = self.parent
         else: error(f"Traversal Error: No Parent Node {self.parent}!")
+
     def traverseDown(self, nodeID, moveTo):
         global location #traverses to one of the child nodes
         if moveTo in range(0,len(self.children)):
             location=
         else: error(f"Index Error: No Child {moveTo}!")
+
     def destroy(self):
-        global IDs
-        IDs=[i for i in IDs if i!=self.nodeID]
-        del self
+        if self.nodeID:
+            global IDs
+            IDs=[i for i in IDs if i!=self.nodeID]
+            del self
 
 # ----------------------------------- Loop ----------------------------------- #
 while True:
     cmd=input("> ")
     try:
         match cmd[0].lower():
-            case "0": #? Commands / Help Menu
+            case _: #? Commands / Help Menu
                 if len(cmd)==1:
                     print(''.join(open("commands/help","r").readlines()[:16]))
                 elif len(cmd)==2:
@@ -81,6 +95,5 @@ while True:
                 pass
             case "f":
                 pass
-            
     except:
         error("Command Error: Unknown Command! (Use \"0\" for help!)")
