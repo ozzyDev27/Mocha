90d3ac10ebc6391bbbd0c318c1402acfb85384e1
ozzy <businessozzy27@gmail.com>
2023-12-06 11:00:00 -0500

    stuff

--- main.py
+++ main.py
@@ -198,8 +198,7 @@ def runCommand(cmd,withinLoop):
             case "6": 
                 openFile(tree[location])
             case "7":
-                print(f"Successfully Created a Branch With Node ID [{next((i for i,j in enumerate(sorted(tree.keys()),start=min(sorted(tree.keys()))) if i!= j), max(sorted(tree.keys()))+1)}]!")
-                newBranch(location)
+                print(f"Successfully Created a Branch With Node ID [{newBranch(location)}]!")
             case "8":
                 if location:
                     if withinLoop or areYouSure():
@@ -281,14 +280,13 @@ def runCommand(cmd,withinLoop):
             case "z": #? debug cmd
                 safetyCheck()
             case _: #? Commands / Help Menu
-                if len(cmd)>1 and cmd.startswith("0"):
-                    if cmd[1].upper() in "0123456789ABCDEF":
-                        if int(cmd[1],16) in range(16):
-                            i,j=open("commands/help","r").readlines()[int(cmd[1],16)].strip().split(",")
-                            print(''.join(open("commands/help","r").readlines()[int(i):int(j)]))
-                        else: error("Unknown Command!","Parameter")
-                    else: print(''.join(open("commands/help","r").readlines()[17:33]))
+                if len(cmd)>1 and cmd.startswith("0") and cmd[1].upper() in "0123456789ABCDEF":
+                    if int(cmd[1],16) in range(16):
+                        i,j=open("commands/help","r").readlines()[int(cmd[1],16)].strip().split(",")
+                        print(''.join(open("commands/help","r").readlines()[int(i):int(j)]))
+                    else: error("Unknown Command!","Parameter")
                 else: print(''.join(open("commands/help","r").readlines()[17:33]))
+
 while running and __name__=="__main__":
     runCommand(input("> "),False)
 print("Goodbye!")
