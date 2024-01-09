c6ba88fe11f2ee9ee5fc7e02b6d3d247df3bcba4
ozzy <businessozzy27@gmail.com>
2024-01-09 09:33:56 -0500

    Update snak.py

--- snak/snak.py
+++ snak/snak.py
@@ -1,2 +1,21 @@
 def runSnak(code):
-    pass
+	lines=code.split("\n")
+	line=0
+	vars={"name":"neel"}
+	while line<len(lines):
+		parts=lines[line].split(" ")
+		if lines[line].strip():
+			match parts[0]:
+				case "dbg":
+					print(' '.join(parts[1:]))
+				case "cmd":
+					pass #runCommand(parts[1],True)
+				case "jmp":
+					line=int(parts[1])-2
+				case "jnz":
+					line=int(parts[1])-2 if vars[parts[2]] else line
+				case "var":
+					match parts[2]:
+						case "int":
+							vars[parts[1]]=int(parts[3])	
+		line+=1
